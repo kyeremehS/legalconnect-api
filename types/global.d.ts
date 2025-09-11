2107161e839ca494d2ee97271de6f12d5bfde831
Sam <affum3331@gmail.com>
2025-09-11 03:31:06 +0000

    feat: Update TypeScript configuration to include DOM library and add global type declarations for Multer file interface

---
+++ types/global.d.ts
@@ -0,0 +1,20 @@
+// Global type declarations
+declare global {
+  namespace Express {
+    namespace Multer {
+      interface File {
+        fieldname: string;
+        originalname: string;
+        encoding: string;
+        mimetype: string;
+        size: number;
+        destination: string;
+        filename: string;
+        path: string;
+        buffer: Buffer;
+      }
+    }
+  }
+}
+
+export {};
